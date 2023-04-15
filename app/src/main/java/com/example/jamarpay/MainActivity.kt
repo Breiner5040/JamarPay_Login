1f692b67f65196caf599754b36f1cc67889aea72
Breiner5040 <bhincapie@jamar.com>
2023-04-15 09:47:47 -0500

    Pantalla de login

--- app/src/main/java/com/example/jamarpay/MainActivity.kt
+++ app/src/main/java/com/example/jamarpay/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.jamarpay
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var mBinding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        val myButton: Button = setContentView(R.id.myButton)
-        myButton.isEnabled = false
 
     }
 
